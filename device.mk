ea72ccf718b2f1c1c0d61dd06da07b3cdc12e178
bheatleyyy <sugakesshaa@pixelos.net>
2022-10-22 02:52:10 +0800

    porsche: Welcome PixelOS
    Co-authored-by: Karan Parashar <karan@pixelos.net>
    Change-Id: I2dabdfcbe557fc0dc563bc0501364865e5c9f6b2

--- device.mk
+++ device.mk
@@ -46,7 +46,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \

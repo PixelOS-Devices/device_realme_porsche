97eb96b618575b81edb4b5e86d20f1dce0c951c8
pjgowtham <pjgowtham@gmail.com>
2023-03-16 10:59:52 +0530

    porsche: Initial rebrand for realme GT 2
    Change-Id: I3273a0d3e34d53414c111d286164e839d5fed8f9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,13 +7,16 @@
 # Include the common OEM chipset BoardConfig.
 include device/oneplus/sm8350-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/oneplus/lemonade
+DEVICE_PATH := device/realme/porsche
 
 # Display
 TARGET_SCREEN_DENSITY := 450
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
+# Partitions
+BOARD_ONEPLUS_DYNAMIC_PARTITIONS_SIZE := 11186208768 # BOARD_SUPER_PARTITION_SIZE - 4MB
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
@@ -22,4 +25,4 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
-include vendor/oneplus/lemonade/BoardConfigVendor.mk
+include vendor/realme/porsche/BoardConfigVendor.mk

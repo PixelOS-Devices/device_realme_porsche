782a3225bc2f21b291ec1f04c1c05b02e6014737
pjgowtham <pjgowtham@gmail.com>
2023-03-16 10:59:52 +0530

    porsche: Initial rebrand for realme GT 2
    Change-Id: I3273a0d3e34d53414c111d286164e839d5fed8f9

---
+++ lineage_porsche.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from porsche device
+$(call inherit-product, device/realme/porsche/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_porsche
+PRODUCT_DEVICE := porsche
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3312
+
+PRODUCT_SYSTEM_NAME := RE58B2L1
+PRODUCT_SYSTEM_DEVICE := RE58B2L1
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="qssi-user 13 TP1A.220905.001 1673407054945 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := realme/RMX3312/RE58B2L1:13/TP1A.220905.001/S.d7a144-1-3cc75:user/release-keys
